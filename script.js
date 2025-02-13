5258c4b2f009a8068c5da4b7144dec5e6348641a
jordanunlawful <JORDANWJ.LA@GMAIL.COM>
2025-02-13 11:31:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function selectOption(option) {
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
+            document.getElementById('message').style.display = 'block'; // Display the message
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
